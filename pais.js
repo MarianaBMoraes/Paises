18269a8c89a82034314d979a482a4a9af39ff9b5
Mariana <marianabmoraes.ef@gmail.com>
2024-07-25 11:28:31 -0300

    refactor criar

--- pais.js
+++ pais.js
@@ -3,16 +3,23 @@ const paises = [];
 
 const validarPais = (pais) => pais.nome != "" && pais.sigla.length == 2;
 
-const criarPais = () => {
+const modelo = () => {
   const nome = prompt("Digite o nome");
   const sigla = prompt("Digite a sigla").toUpperCase();
 
   if (validarPais({ nome, sigla })) {
-    paises.push({ nome, sigla });
-    console.log("País criado com sucesso!");
-  } else {
-    console.log("País inválido!")
-  }
+      paises.push({ nome, sigla });
+      console.log("País criado com sucesso!");
+    } else {
+      console.log("País inválido!");
+    }
+  };
+
+
+const criarPais = () => {
+  const pais = modelo()
+
+  if (pais != undefined){
+    
+  } 
 }
-
-
